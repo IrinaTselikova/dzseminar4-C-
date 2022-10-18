474f3fd663fca9c1dd981518b20ce5080d53763d
unknown <hurina.irina@mail.ru>
2022-10-18 19:26:41 +0300

    jdvffdsd

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+ввод элеменетов через консоль (через запятую. использовать 
+метод string.Split()) */
+
+string GetNumber(string message)
+{
+    Console.WriteLine(message);
+    string result = Console.ReadLine();
+    return result;
+}
+
+string str = GetNumber("Введите 8 цифр через запятую:");
+string[] numbers = str.Split(',');
+foreach (var number in numbers)
+
+
+Console.Write($"{number[1]}");
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
